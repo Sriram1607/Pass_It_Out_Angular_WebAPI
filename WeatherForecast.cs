763b097bbdaf2f688d26c91dcf4071bd30aa75a8
Sriram Maroju <48040081+Sriram1607@users.noreply.github.com>
2024-02-20 11:56:39 +0530

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Pass_It_Out_Angular_WebAPI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
